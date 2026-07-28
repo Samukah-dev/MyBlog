8c7951ebd5640a572cadca385e4736956fd0443f
Samukah.dev <samuel.csta.stos@gmail.com>
2026-07-28 16:57:24 -0300

    Add Firebase Authentication import and initialization

--- js/firebase.js
+++ js/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js';
+import { getAuth } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js';
 
 const firebaseConfig = {
@@ -10,4 +11,5 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
 export const db = getFirestore(app);
